cc908d5585f70cc337f62041c9399b70c6233f7c
Jose Manuel Diaz Diaz <Jose.Diaz@unidadvictimas.gov.co>
2018-10-18 18:37:27 -0500

    Ajuste de menú

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,18 +32,18 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   pages: any[] = [
-    { title: 'Tutorial', component: 'TutorialPage' },
-    { title: 'Welcome', component: 'WelcomePage' },
-    { title: 'Tabs', component: 'TabsPage' },
+    { title: 'Ayuda', component: 'TutorialPage' },
+    { title: 'Inicio', component: 'WelcomePage' },
+    /*{ title: 'Tabs', component: 'TabsPage' },
     { title: 'Cards', component: 'CardsPage' },
-    { title: 'Content', component: 'ContentPage' },
-    { title: 'Login', component: 'LoginPage' },
-    { title: 'Signup', component: 'SignupPage' },
-    { title: 'Master Detail', component: 'ListMasterPage' },
+    { title: 'Content', component: 'ContentPage' },*/
+    { title: 'Ingresar', component: 'LoginPage' },
+    { title: 'Registrarse', component: 'SignupPage' },
+    /*{ title: 'Master Detail', component: 'ListMasterPage' },
     { title: 'Menu', component: 'MenuPage' },
     { title: 'Settings', component: 'SettingsPage' },
-    { title: 'Search', component: 'SearchPage' },
-    { title: 'Mapas', component: 'MapaPrincipalPage' }
+    { title: 'Search', component: 'SearchPage' },*/
+    { title: 'Mapa De Delincuencia', component: 'MapaPrincipalPage' }
   ]
 
   constructor(private translate: TranslateService, 
